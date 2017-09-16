8a20097200ea7736ac7e2d1bb4866f6fe45b059c
Govind <govind96@users.noreply.github.com>
2017-09-17 05:29:04 +0530

    Update dank_wallpaper.py

--- dank_wallpaper.py
+++ dank_wallpaper.py
@@ -1,21 +1,26 @@
+from os import system
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw 
-import os
-import PIL
+from random import randint
+
 
 font = ImageFont.truetype("/usr/share/fonts/tlwg/Purisa.ttf", 40)
-img = Image.new("RGBA", (1920,1080), (200,20,20))
+r = randint(0, 256)
+g = randint(0, 256)
+b = randint(0, 256)
+img = Image.new("RGBA", (1920, 1080), (r, g, b))
 draw = ImageDraw.Draw(img)
 width = 1080
 height = 1920
 x = 0
 y = 0
+r = randint(0, 256)
+g = randint(0, 256)
+b = randint(0, 256)
 for x in range(0, height, 120):
     for y in range(0, width, 120):
-        draw.text((x,y), "LoL", (255,255,0), font=font)
+        draw.text((x,y), "LoL", (r, g, b), font = font)
 
 draw = ImageDraw.Draw(img)      
 img.save("/home/user/Pictures/you_can_not_escape.png")
-
-os.system("gsettings set org.mate.background picture-filename /home/user/Pictures/you_can_not_escape.png")
