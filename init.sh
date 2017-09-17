ac7ece26b6d7f8c7f87fee562c06490e9702bedd
Govind <govind96@users.noreply.github.com>
2017-09-17 05:43:12 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,3 +5,6 @@ export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/en
 wget https://raw.githubusercontent.com/govind96/lol_wallpaper/master/dank_wallpaper.py
 python dank_wallpaper.py
 gsettings set org.mate.background picture-filename /home/user/Pictures/you_can_not_escape.png
+rm dank_wallpaper.py
+sleep 1.4
+rm /home/user/Pictures/you_can_not_escape.png
