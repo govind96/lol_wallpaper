f6f92ad2dc524c128dc888606ead42b3f827b347
Govind <govind96@users.noreply.github.com>
2017-09-18 19:30:32 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,8 @@ PID=$(pgrep mate-session)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 wget https://raw.githubusercontent.com/govind96/lol_wallpaper/master/dank_wallpaper.py
 python dank_wallpaper.py
-gsettings set org.mate.background picture-filename /home/user/Pictures/you_can_not_escape.png
+gsettings set org.mate.background picture-filename /home/user/.config/you_can_not_escape.png
 rm dank_wallpaper.py
-sleep 1.4
-rm /home/user/Pictures/you_can_not_escape.png
+sleep 5
+#commenting now to see the effect
+#rm /home/user/Pictures/you_can_not_escape.png
