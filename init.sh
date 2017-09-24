959947855d7218493c95c94daccb278a73083d6b
Govind <govind96@users.noreply.github.com>
2017-09-24 21:47:12 +0530

    Update init.sh
    A little change, since password has been changed by Amit.

--- init.sh
+++ init.sh
@@ -2,10 +2,11 @@
 
 PID=$(pgrep mate-session)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
-wget https://raw.githubusercontent.com/govind96/lol_wallpaper/master/dank_wallpaper.py
-python dank_wallpaper.py
+wget https://raw.githubusercontent.com/govind96/lol_wallpaper/master/dank_wallpaper.py -P /home/user/Downloads
+python /home/user/Downloads/dank_wallpaper.py
+sleep 2
 gsettings set org.mate.background picture-filename /home/user/.config/you_can_not_escape.png
-rm dank_wallpaper.py
+#rm /home/user/Downloads/dank_wallpaper.py
 sleep 5
 #commenting now to see the effect
 #rm /home/user/Pictures/you_can_not_escape.png
